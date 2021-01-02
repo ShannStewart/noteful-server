07a3b67e8b722e22491ef5acff18e3ffa69e9431
ShannStewart <Zackry.Broodie.Stewart@gmail.com>
2021-01-02 18:27:23 -0500

    online

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/noteful',
+    DB_URL: process.env.DB_URL || 'postgres://rwdhntkhuroptu:48522106b9f5aaf0890905df7abe67c75710e00aba50b80edcf01a07d5cbe918@ec2-52-203-49-58.compute-1.amazonaws.com:5432/dcgq78oje0lmb6',
   }
