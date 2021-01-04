ea597c35a67fc21ff9df01c50ee51ee0b2af4c44
ShannStewart <Zackry.Broodie.Stewart@gmail.com>
2021-01-04 15:26:31 -0500

    post

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": process.env.DB_URL,
+  "connectionString":  "dbname=dcgq78oje0lmb6 host=ec2-52-203-49-58.compute-1.amazonaws.com port=5432 user=rwdhntkhuroptu password=48522106b9f5aaf0890905df7abe67c75710e00aba50b80edcf01a07d5cbe918 sslmode=require",
 }
