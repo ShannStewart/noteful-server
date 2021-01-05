79da9658c23ff6c6c8292b642c4c2eebca6d62c1
ShannStewart <Zackry.Broodie.Stewart@gmail.com>
2021-01-05 16:26:20 -0500

    honestsly dunno

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": process.env.DATABASE_URL || 'postgres://rwdhntkhuroptu:48522106b9f5aaf0890905df7abe67c75710e00aba50b80edcf01a07d5cbe918@ec2-52-203-49-58.compute-1.amazonaws.com:5432/dcgq78oje0lmb6'
+  "connectionString": process.env.DATABASE_URL,
 }
